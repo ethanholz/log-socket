b65a10e7a8a1685c6f20842975fe1b5372ad1846
Tai Groot <tai@taigrr.com>
2021-07-05 16:07:50 -0700

    detect tls presence based on incoming request for websocket

--- browser/browser.go
+++ browser/browser.go
@@ -7,7 +7,12 @@ import (
 )
 
 func LogSocketViewHandler(w http.ResponseWriter, r *http.Request) {
-	wsResource := "ws://" + r.Host + r.URL.Path
+	wsResource := r.Host + r.URL.Path
+	if r.TLS != nil {
+		wsResource = "wss://" + wsResource
+	} else {
+		wsResource = "ws://" + wsResource
+	}
 	wsResource = strings.TrimSuffix(wsResource, "/") + "/ws"
 	homeTemplate.Execute(w, wsResource)
 }
